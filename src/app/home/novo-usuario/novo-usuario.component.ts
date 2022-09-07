3d6590443ff212ea4423b794904382439b0c6174
pedroarneiroz <pedroarneiroz@gmail.com>
2022-09-07 11:04:31 -0300

    Mais algumas validaçoes no form e verificação se o usuario já está cadastrado

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { minusculoValidator } from './minuscilo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
+import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais-validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -12,20 +16,35 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+  constructor(private formBuilder: FormBuilder, 
+              private novoUsuarioService: NovoUsuarioService,
+              private usuarioExistenteService: UsuarioExisteService,
+              private router: Router,
+              ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      fullName: [''],
-      email: [''],
-      userName: [''],
+      fullName: ['', Validators.required, Validators.minLength(4)],
+      email: ['', [Validators.required, Validators.email, ]],
+      userName: ['', [minusculoValidator], [this.usuarioExistenteService.usuarioJaExiste()]],
       password: [''],
-    });
+    },{
+      Validators: [usuarioSenhaIguaisValidator],
+    }
+
+    );
   }
 
   cadastrar(){
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if(this.novoUsuarioForm.valid){
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate([''])
+      },
+      (error) => {
+        console.log(error);
+      }
+      );
+    }    
   }
-
 }
